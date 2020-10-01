70ba1404ebad643b00bd9dac907351834f6a8a25
thoughtless02 <62426189+thoughtless02@users.noreply.github.com>
2020-10-01 23:45:05 +0530

    created pymongo db

--- tweepy-bot/bot.py
+++ tweepy-bot/bot.py
@@ -2,12 +2,17 @@
 import tweepy
 import requests
 import json
-
+import pymongo
 # personal details
 consumer_key ="dKRVYTRotNt0Xe2Cy3oFfhybw"
 consumer_secret ="AcRg9XqEe3x2bZO3C2Ce4rpd7KvuTtvj3dtsguU6iNlLY79v11"
 access_token ="1311544857941274626-L8zWYVKwL8p4glQhvY1dD4RdHSLG6r"
 access_token_secret ="BcdkoB33j0MxSZDiYylW1AE1K5aQaDcp9Rjip4aCBHYpb"
+
+# creating a MongoClient
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
 
 
 # authentication of consumer key and secret
@@ -27,9 +32,12 @@ def humanize_url(url):
     h_url = url[:8]+"github.com/"+url[29:]
     return h_url
 
-for i in range(0,30):
+
+for i in range(0,len(all_items)):
+    url = humanize_url(all_items[i]["url"])
+    dict = {"url":url}
     print(i+1)
-    print(all_items[i]["title"] + "\n" + humanize_url(all_items[i]["url"]))
+    print(all_items[i]["title"] + "\n" + url)
     print("\n")
 
 
